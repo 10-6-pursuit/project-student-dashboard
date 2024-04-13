f7c109feb11a3107fc44838789b04b84b4b6c3c3
AntCannon <anthonycannonier@gmail.com>
2024-04-13 12:52:38 -0400

    add student progress sections codewars, scores and certifications

--- src/Components/StudentDetails.jsx
+++ src/Components/StudentDetails.jsx
@@ -1,9 +1,17 @@
 export default function StudentDetails() {
   
   return (
-    <section className="details-section">
-      <div className="student-details">
-          Student Details
+    <section className="student-details">
+      <div className="student-progress">
+        <div className="codewars">
+          <h4>CodeWars:</h4>
+        </div>
+        <div className="Scores">
+          <h4>Scores:</h4>
+        </div>
+        <div className="Certifications">
+          <h4>Certifications:</h4>
+        </div>
       </div>
       <hr className="green"/>
       <div className="student-notes">
