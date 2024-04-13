7b8ab557f66d665cf1d48ef966fb04daa718b233
AntCannon <anthonycannonier@gmail.com>
2024-04-13 11:38:15 -0400

    add static data

--- src/Components/StudentList.jsx
+++ src/Components/StudentList.jsx
@@ -2,7 +2,21 @@ export default function StudentList() {
 
   return (
     <section className="student-list">
-      <h2>StudentList</h2>
+      <h2>Selected Cohort Name</h2>
+      <span>
+        <span className="green">Total Students: </span>numStudents
+      </span>
+      <div className="student-card">
+        <div className="avatar" src="">avatar</div>
+        <h3>First M. Last</h3>
+        <span>studentEmail</span>
+        <br />
+        <span>
+          <span className="green">Birthday:</span> studentBirthday
+        </span>
+        <br />
+        <span className="green">Show More...</span>
+      </div>
     </section>
 
   )
