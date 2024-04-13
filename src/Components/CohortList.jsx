87e12d6e95a8d614435794854f2c0e576280da4c
AntCannon <anthonycannonier@gmail.com>
2024-04-13 15:23:57 -0400

    update style

--- src/Components/CohortList.jsx
+++ src/Components/CohortList.jsx
@@ -2,7 +2,7 @@ export default function CohortList() {
 
   return (
     <section className="cohort-list">
-      <h2>CohortList</h2>
+      <h2>Choose a Class by Start Date</h2>
       <p>(all cohorts most recent to less recent)</p>
       <h3>cohort 1</h3>
       <hr />
