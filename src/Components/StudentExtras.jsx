843002e2d71318f218c5cdb844cd501725271328
gerardjr42 <ggarciajr12@gmail.com>
2024-04-13 01:03:30 -0400

    Create Form and Style

--- src/Components/StudentExtras.jsx
+++ src/Components/StudentExtras.jsx
@@ -1,69 +1,110 @@
-export default function StudentExtras({studentID, codewars, certifications, scores}) {
+export default function StudentExtras({
+  studentID,
+  codewars,
+  certifications,
+  scores,
+}) {
   //!Need state type boolean for the certifications
   //!Need a function to decide what emoji to add depending if certification: if true add check, if false add x
 
   //Deconstruct codewars object
-  const {current, goal} = codewars;
-  const {total: currentTotal, lastWeek: currentLastWeek} = current;
-  const {total: goalTotal, lastWeek: goalLastWeek} = goal;
+  const { current, goal } = codewars;
+  const { total: currentTotal, lastWeek: currentLastWeek } = current;
+  const { total: goalTotal, lastWeek: goalLastWeek } = goal;
 
- const percentGoal = Math.round(((currentTotal / goalTotal) * 100));
+  const percentGoal = Math.round((currentTotal / goalTotal) * 100);
 
   // Deconstruct scores object
-  const {assignments: assignment, projects: project, assessments: assessment} = scores;
+  const {
+    assignments: assignment,
+    projects: project,
+    assessments: assessment,
+  } = scores;
 
   return (
-    <div className="extra-info" key={studentID}>
-
-      <div className="codewars">
-        <h3>CodeWars:</h3>
-        <ul>
-          <li><span>Current Total: </span>{currentTotal}</li>
-          <li><span>Last Week: </span>{currentLastWeek}</li>
-          <li><span>Goal: </span>{goalTotal}</li>
-          <li><span>Percent of Goal Achieved: </span>{percentGoal}%</li>
-        </ul>
-      </div>
-
-      <div className="scores">
-        <h3>Scores</h3>
-        <ul>
-          <li><span>Assignment: </span>{assignment * 100}%</li>
-          <li><span>Projects: </span>{project * 100}%</li>
-          <li><span>Assessments:</span>{assessment * 100}%</li>
-        </ul>
-      </div>
-
-      <div className="certifications">
-        <h3>Certifications:</h3>
-        <ul>
-          <li><span>Resume:</span></li>
-          <li><span>LinkedIn:</span></li>
-          <li><span>Mock Interview:</span></li>
-          <li><span>Github:</span></li>
-        </ul>
-      </div>
-      <hr/>
-      <div className="note">
-      <h3>1-on-1 Notes</h3>
-        <div className="inputs">
-          <label htmlFor="comment-name">
-            Comment Name
-            <input type="text" id="comment-name" name="comment-name"/>
-          </label>
-
-          <label htmlFor="comment">
-            Comment
-            <input type="text" id="comment" name="comment"/>
-          </label>
-
-          <button>Add Note</button>
-
-          //!Below Here add a Component Called Notes that will get the value of the inputs and place them here as a ul
-
+    <div className="extras">
+      <div className="extra-info" key={studentID}>
+        <div className="codewars">
+          <h3>CodeWars:</h3>
+          <ul>
+            <li>
+              <span>Current Total: </span>
+              {currentTotal}
+            </li>
+            <li>
+              <span>Last Week: </span>
+              {currentLastWeek}
+            </li>
+            <li>
+              <span>Goal: </span>
+              {goalTotal}
+            </li>
+            <li>
+              <span>Percent of Goal Achieved: </span>
+              {percentGoal}%
+            </li>
+          </ul>
+        </div>
+        <div className="scores">
+          <h3>Scores</h3>
+          <ul>
+            <li>
+              <span>Assignment: </span>
+              {assignment * 100}%
+            </li>
+            <li>
+              <span>Projects: </span>
+              {project * 100}%
+            </li>
+            <li>
+              <span>Assessments:</span>
+              {assessment * 100}%
+            </li>
+          </ul>
+        </div>
+        <div className="certifications">
+          <h3>Certifications:</h3>
+          <ul>
+            <li>
+              <span>Resume:</span>
+            </li>
+            <li>
+              <span>LinkedIn:</span>
+            </li>
+            <li>
+              <span>Mock Interview:</span>
+            </li>
+            <li>
+              <span>Github:</span>
+            </li>
+          </ul>
         </div>
       </div>
+      <div className="note">
+        {/* move hr up if not shown */}
+        <hr />
+        <section className="form-note">
+          <h3>1-on-1 Notes</h3>
+          <form action="">
+            <div className="inputs">
+              <fieldset>
+                <label htmlFor="comment-name">Comment Name
+                  <input type="text" id="comment-name"name="comment-name" />
+                </label>
 
+                <label htmlFor="comment">Comment
+                  <input type="text" id="comment" name="comment" />
+                </label>
+
+                <button>Add Note</button>
+              </fieldset>
+            </div>
+          </form>
+        </section>
+        <ul>
+          <li>Add Note Here</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
