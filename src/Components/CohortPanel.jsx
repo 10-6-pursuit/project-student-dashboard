0904a325cb89c724b35038b11cc3aa8f5afa0ca7
AntCannon <anthonycannonier@gmail.com>
2024-04-13 16:11:09 -0400

    add Cohorts.jsx adjust relationships

--- src/Components/CohortPanel.jsx
+++ src/Components/CohortPanel.jsx
@@ -1,15 +1,11 @@
-import Cohort from './Cohort.jsx'
+import Cohorts from './Cohorts.jsx'
 
 export default function CohortPanel() {
 
   return (
     <section className="cohort-panel">
       <h2>Choose a Class by Start Date</h2>
-      <div className="cohort-list">
-        <Cohort />
-        <Cohort />
-        <Cohort />
-      </div>
+      <Cohorts />
     </section>
 
   )
---
+++ src/Components/Cohorts.jsx
@@ -0,0 +1,12 @@
+import Cohort from './Cohort.jsx'
+
+export default function Cohorts() {
+
+  return (
+    <div className="cohort-list">
+      <Cohort />
+      <Cohort />
+      <Cohort />
+  </div>
+  )
+}
