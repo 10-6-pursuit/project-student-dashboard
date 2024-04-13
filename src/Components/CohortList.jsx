4fc03942e247fe981c5497db2078ddd09b38aa6f
AntCannon <anthonycannonier@gmail.com>
2024-04-13 11:29:10 -0400

    add static data

--- src/Components/CohortList.jsx
+++ src/Components/CohortList.jsx
@@ -2,7 +2,14 @@ export default function CohortList() {
 
   return (
     <section className="cohort-list">
-      <h2>cohortList</h2>
+      <h2>CohortList</h2>
+      <p>(all cohorts most recent to less recent)</p>
+      <h3>cohort 1</h3>
+      <hr />
+      <h3>cohort 2</h3>
+      <hr />
+      <h3>cohort 3</h3>
+      <hr />
     </section>
 
   )
