db5fb150d992e08c9cdfacaaac041062eb9cd5c9
AntCannon <anthonycannonier@gmail.com>
2024-04-13 11:24:07 -0400

    add StudentList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from  './Components/Header.jsx'
 import CohortList from  './Components/CohortList.jsx'
+import StudentList from  './Components/StudentList.jsx'
+
 function App() {
   return (
     <>
       <Header />
       <main>
         <CohortList />
-        <section className="student-list">studentList</section>
+        <StudentList />
       </main>
     </>
   );
---
+++ src/Components/StudentList.jsx
@@ -0,0 +1,9 @@
+export default function StudentList() {
+
+  return (
+    <section className="student-list">
+      <h2>StudentList</h2>
+    </section>
+
+  )
+}
