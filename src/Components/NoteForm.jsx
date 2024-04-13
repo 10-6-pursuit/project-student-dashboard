6fd13ebca9a5c69b961f94b71f5bfc315a6674d5
AntCannon <anthonycannonier@gmail.com>
2024-04-13 15:05:12 -0400

    add NoteForm.jsx

---
+++ src/Components/NoteForm.jsx
@@ -0,0 +1,16 @@
+export default function NoteForm() {
+
+  return(
+    <form className="notes-form">
+      <fieldset>
+        <label htmlFor="commenter-name">Commenter Name: </label>
+        <input type="text" id="commenterName" required/>
+        <br />
+        <label htmlFor="comment">Commenter: </label>
+        <input type="text" id="comment" required/>
+        <br />
+        <button type="submit">Add Note</button>
+      </fieldset>
+    </form>
+  )
+}
--- src/Components/NotesSection.jsx
+++ src/Components/NotesSection.jsx
@@ -1,19 +1,12 @@
+import NoteForm from './NoteForm.jsx'
+
+
 export default function NotesSection() {
 
   return (
     <div className="student-notes">
       1-On-1 Notes
-      <form className="notes-form">
-        <fieldset>
-          <label htmlFor="commenter-name">Commenter Name: </label>
-          <input type="text" id="commenterName" required/>
-          <br />
-          <label htmlFor="comment">Commenter: </label>
-          <input type="text" id="comment" required/>
-          <br />
-          <button type="submit">Add Note</button>
-        </fieldset>
-      </form>
+      <NoteForm />
       <ul className="notes">
         <li>COMMENTER N. says "NOTE"</li>
       </ul>
