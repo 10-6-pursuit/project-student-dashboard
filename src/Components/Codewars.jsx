d591832df0838a030799616818842310cfc6b3ba
AntCannon <anthonycannonier@gmail.com>
2024-04-14 11:58:11 -0400

    make dynamic

--- src/Components/Codewars.jsx
+++ src/Components/Codewars.jsx
@@ -1,22 +1,35 @@
-export default function Codewars() {
+export default function Codewars({ codewars }) {
+  const {
+    current: {
+      total: cTotal,
+      lastWeek: cLastWeek
+    },
+    goal: {
+      total: gTotal,
+      lastWeek: gLastWeek
+    }
+  } = codewars;
+
+  // percent goal achieved
+  const percentGoalAchieved = (cTotal / gTotal * 100).toFixed() 
 
   return (
     <div className="codewars">
       <h4>CodeWars:</h4>
       <span>
-        <span className="green">Current Total: </span>POINTS
+        <span className="green">Current Total: </span>{cTotal}
       </span>
       <br />
       <span>
-        <span className="green">Last Week: </span>POINTS
+        <span className="green">Last Week: </span>{cLastWeek}
       </span>
       <br />
       <span>
-        <span className="green">Goal: </span>POINTS
+        <span className="green">Goal: </span>{gTotal}
       </span>
       <br />
       <span>
-        <span className="green">Percent of Goal Achieved: </span>POINTS
+        <span className="green">Percent of Goal Achieved: </span>{percentGoalAchieved}%
       </span>
       <br />
     </div>
