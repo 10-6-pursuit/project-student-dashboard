d01240e5b62f594fc71c1294875948593865ec82
AntCannon <anthonycannonier@gmail.com>
2024-04-14 12:57:23 -0400

    add dynamic note count

--- src/Components/NotesSection.jsx
+++ src/Components/NotesSection.jsx
@@ -2,13 +2,13 @@ import NoteForm from './NoteForm.jsx'
 import Notes from './Notes.jsx'
 
 
-export default function NotesSection() {
+export default function NotesSection({ notes }) {
 
   return (
     <div className="student-notes">
       <h3>1-On-1 Notes</h3>
       <span>
-        Total Notes: <span className="green">#NOTES</span>
+        Total Notes: <span className="green">{notes.length}</span>
       </span>
       <NoteForm />
       <Notes />
