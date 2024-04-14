25c3cbe977d760c88b2625baf65095fe0f289cf7
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-04-14 17:04:55 -0400

    mapped filter buttons instead of hard code

--- src/components/CohortList.jsx
+++ src/components/CohortList.jsx
@@ -47,54 +47,15 @@ export default function CohortList() {
         >
           All Students
         </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Winter2026")}
-        >
-          Winter 2026
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Fall2026")}
-        >
-          Fall 2026
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Summer2026")}
-        >
-          Summer 2026
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Spring2026")}
-        >
-          Spring 2026
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Winter2025")}
-        >
-          Winter 2025
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Fall2025")}
-        >
-          Fall 2025
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Summer2025")}
-        >
-          Summer 2025
-        </button>
-        <button
-          className="cohort-list__container__filter-btn"
-          onClick={() => handleFilter("Spring2025")}
-        >
-          Spring 2025
-        </button>
+        {["Winter2026", "Fall2026", "Summer2026", "Spring2026", "Winter2025", "Fall2025", "Summer2025", "Spring2025"].map(cohort => (
+          <button
+            key={cohort}
+            className="cohort-list__container__filter-btn"
+            onClick={() => handleFilter(cohort)}
+          >
+            {handleFilterTitle(cohort)}
+          </button>
+        ))}
       </div>
       <StudentList
         filter={filter}
