5f54f4b617cf2e9ac93247ea2c42fb4863c7d4b7
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-04-17 19:11:48 -0400

    Toggle dark mode functionality working (50% missing styling)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import CohortList from "./components/CohortList";
 import "/src/index.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div id="app">
+    <div id="app" className={darkMode ? 'dark-mode' : ''}>
       <h1>Student Dashboard</h1>
+      <button className="toggle-btn" onClick={toggleDarkMode}>
+        Toggle Dark Mode
+      </button>
       <CohortList />
     </div>
   );
