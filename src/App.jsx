0975a38ef3a758ab8eecdef572b7aec165410fff
PRRInc <prrincdev@gmail.com>
2024-04-16 21:25:43 -0400

    Initial App.jsx Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import CohortList from "./Components/CohortList.jsx"
+import StudentList from  './Components/StudentList.jsx'
+import studentInfoArray from './data/data.json'
+
+
+
 
 function App() {
+  const [students, setStudents] = useState(studentInfoArray);
+  const [filteredStudents, setFilteredStudents] = useState(students);
+  const [cohort, setCohort] = useState("All Students");
+  
+
+
+
   return (
     <div>
       <h1>Student Dashboard</h1>
