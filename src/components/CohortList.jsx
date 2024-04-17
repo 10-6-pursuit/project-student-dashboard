a6159ed4beacbe2c1ff8e58ffe380dc22c59c0e9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-04-17 19:53:45 -0400

    complete stretch goal of color coding the cohort list

--- src/components/CohortList.jsx
+++ src/components/CohortList.jsx
@@ -2,10 +2,23 @@ import "/src/components/CohortList.css"
 
 export default function CohortList({cohortSelection, handleCohortChange}) {
 
+   const colorObj = {
+    ["Winter 2026"] : "red",
+    ["Fall 2026"] : "yellow",
+    ["Summer 2026"] : "pink",
+    ["Spring 2026"] : "blue",
+    ["Winter 2025"] : "purple",
+    ["Fall 2025"] : "grey",
+    ["Summer 2025"] : "orange",
+    ["Spring 2025"] : "green"
+   }
+
+   console.log(colorObj)
+
     return (
         <div className="cohortList__card">
             <h2>Choose A Class by Start Date</h2>
-            {cohortSelection.map((cohort,i) => <li key={i}><input type="button" onClick={handleCohortChange} value={cohort} /></li>)}
+            {cohortSelection.map((cohort,i) => <li key={i}><input style={{color:colorObj[cohort] ? colorObj[cohort]: "black"}} type="button" onClick={handleCohortChange} value={cohort} /></li>)}
         </div>
     )
 }
