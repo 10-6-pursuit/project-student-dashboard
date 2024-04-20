9d79fcce782ae1be8a98852f0948e5932434717b
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-04-20 15:55:13 -0400

    moved component, refactored app.jsx

--- src/components/CohortList.jsx
+++ src/components/CohortList.jsx
@@ -1,42 +1,15 @@
-import React, { useState } from "react";
-import StudentList from "./StudentList";
 import "/src/styles/CohortList.css";
 
-export default function CohortList({ darkMode }) {
-  const [filter, setFilter] = useState(null);
-  const [comments, setComments] = useState({});
-
-  const handleFilter = (cohort) => setFilter(cohort);
-  const handleShowAll = () => setFilter(null);
-  const handleFilterTitle = (input) => {
-    const obj = {
-      Winter2026: "Winter 2026",
-      Fall2026: "Fall 2026",
-      Summer2026: "Summer 2026",
-      Spring2026: "Spring 2026",
-      Winter2025: "Winter 2025",
-      Fall2025: "Fall 2025",
-      Summer2025: "Summer 2025",
-      Spring2025: "Spring 2025",
-    };
-
-    return obj[input] ? obj[input] : input;
-  };
-  
-  const addComment = (studentId, commenter, comment) => {
-    setComments((prevComments) => ({...prevComments, [studentId]: [...(prevComments[studentId] || []), { commenter, comment }]}));
-  };
-
+export default function CohortList({ darkMode, handleFilter, handleShowAll, handleFilterTitle }) {
   return (
     <div className="cohort-list">
       <div className="cohort-list__container">
-        <h2 className="cohort-list__container__filter-title">{filter ? `Showing ${handleFilterTitle(filter)}` : "Choose a Class by Start Date"}</h2>
-        <button className={`cohort-list__container__filter-btn ${darkMode ? "dark-mode" : ""}`} onClick={handleShowAll}> All Students </button>
-        {["Winter2026", "Fall2026", "Summer2026", "Spring2026", "Winter2025", "Fall2025", "Summer2025", "Spring2025",].map((cohort) => (
-          <button key={cohort} className={`cohort-list__container__filter-btn ${darkMode ? "dark-mode" : ""}`} onClick={() => handleFilter(cohort)}>{handleFilterTitle(cohort)}</button>
+        <h2 className="cohort-list__container__filter-title">Choose a Class by Start Date</h2>
+        <button className={`cohort-list__container__filter-btn ${darkMode ? "dark-mode" : "light-mode"}`} onClick={handleShowAll}> All Students </button>
+        {["Winter2026", "Fall2026", "Summer2026", "Spring2026", "Winter2025", "Fall2025", "Summer2025", "Spring2025"].map((cohort) => (
+          <button key={cohort} className={`cohort-list__container__filter-btn ${darkMode ? "dark-mode" : "light-mode"}`} onClick={() => handleFilter(cohort)}>{handleFilterTitle(cohort)}</button>
         ))}
       </div>
-      <StudentList filter={filter} comments={comments} addComment={addComment} handleFilterTitle={handleFilterTitle} darkMode={darkMode}/>
     </div>
   );
 }
